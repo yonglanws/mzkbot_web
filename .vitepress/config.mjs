57b9173a7bac95adf7572c0905622188ec0bfb3d
慵懒午睡 <124489556+yonglanws@users.noreply.github.com>
2026-03-02 04:29:09 +0800

    Add files via upload

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "MIZUKI BOT",
-  description: "MIZUKI BOT",
+  title: "mzkbot帮助文档",
+  description: "mzkbot帮助文档",
   base: "/mzkbot_web/",
+  head: [["link", { rel: "icon", href: "https://bot-assets.xszq.xyz/meme/pjsk/Mizuki/Mizuki_03.webp" }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -13,22 +14,22 @@ export default defineConfig({
       { text: '在线状态', link: '/status' }
 
     ],
-
+    footer:{
+      copyright: "© Copyright 2026 慵懒午睡"
+    },
     sidebar: [
       {
         text: '使用指南',
         items: [
           { text: '如何使用', link: '/how2use' },
-          { text: '指令帮助', link: '/help' }
+          { text: '指令帮助', link: '/help' },
+          { text: '使用BOT的规范', link: '/guize' }
         ]
       },
       {
-        text: '关于与规范',
+        text: '关于',
         items: [
-          { text: '关于', link: '/about' },
           { text: '开发者&鸣谢', link: '/thank' },
-          { text: '使用BOT的规范', link: '/guize' },
-          { text: '在线状态', link: '/status' },
           { text: '赞助', link: '/money' }
         ]
       }
@@ -37,8 +38,6 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/yonglanws/mzkbot_web' }
     ]
-  },
-  footer:{
-    copyright: "© Copyright 2026 慵懒午睡"
   }
+  
 })
